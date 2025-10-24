411682376b1ad3cceec1ab80c52f48021e8f45fb
BhaskarChhabra <166388421+BhaskarChhabra@users.noreply.github.com>
2025-10-25 01:59:50 +0530

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -38,7 +38,7 @@ router.get(
     res
       .cookie("token", token, {
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV === "production",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
